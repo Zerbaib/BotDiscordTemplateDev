bdb7574555349282bdb99855e004fe845a4e00c8
Zerbaib <hugo.hennetin@proton.me>
2023-03-05 19:27:52 +0100

    patch

--- cogs/rank.py
+++ cogs/rank.py
@@ -79,7 +79,7 @@ class Rank(commands.Cog):
 
         if xp >= xp_required:
             self.data[guild_id][user_id]["level"] = lvl + 1
-            embed = disnake.Embed(title=f"{message.author.name} Vien juste de gagner **1** niveau !",
+            embed = disnake.Embed(title=f"{message.author.name} Vient juste de gagner **1** niveau !",
                                   description=f"Bravo {message.author.mention} ! Tu viens d'arriver au niveau {lvl + 1}!",
                                   color=config.Success())
             await message.channel.send(embed=embed, delete_after=10)
